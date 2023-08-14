eb424b1868085349d97c6e5323713fa2833f8981
Aiden Campbell <Campbell.e.aiden@gmail.com>
2023-08-14 22:29:42 +0000

    wip

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import MenuItem from "./MenuItem"
 import { AiFillHome } from 'react-icons/ai'
 import { BsInfoCircleFill } from 'react-icons/bs'
@@ -11,7 +12,7 @@ const Header = () => {
             </div>
            <div className="">
                 <Link href="/">
-                    <h2><span></span><span></span></h2>
+                    <h2 className="text-2xl"><span className="font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDb</span><span className="text-xl sm:inline hidden">Clone</span></h2>
                 </Link>
            </div>
         </div>
