b7738f529b49a1a440a1df4a9388271bcef2db8a
Aiden Campbell <Campbell.e.aiden@gmail.com>
2023-08-15 09:37:48 +0000

    headder

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,14 +5,17 @@ import { BsInfoCircleFill } from 'react-icons/bs'
 
 const Header = () => {
     return (
-        <div className="">
+        <div className="flex justify-between mx-2 sm:mx-auto max-w-6xl items-center py-6">
            <div className="flex">
                 <MenuItem title="HOME" Icon={AiFillHome} path="/home" />
                 <MenuItem title="ABOUT" Icon={BsInfoCircleFill} path="/about" />
             </div>
            <div className="">
                 <Link href="/">
-                    <h2 className="text-2xl"><span className="font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDb</span><span className="text-xl sm:inline hidden">Clone</span></h2>
+                    <h2 className="text-2xl">
+                        <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDb</span>
+                        <span className="text-xl sm:inline hidden">Clone</span>
+                    </h2>
                 </Link>
            </div>
         </div>
